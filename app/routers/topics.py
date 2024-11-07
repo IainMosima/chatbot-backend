dcab5f82bf8f319b7f44916cda8c2dbb7d5479b4
Iain Mosima <iain.nyakundi@apollo.ac.ke>
2024-11-07 20:23:38 +0300

    working on routes

--- app/routers/topics.py
+++ app/routers/topics.py
@@ -9,8 +9,12 @@ router = APIRouter(
     tags=["topic"],
 )
 
-# /all-topics?userId=1
+# /topic-titles?userId=1
 @router.get("/topic-titles")
 async def get_topics(userId, db=Depends(get_db)):
     topics = await db.topics.find({"userId": userId}).to_list(length=100)
     return [topic["title"] for topic in topics]
+
+@router.post("/")
+def create_topic(title: str, db=Depends(get_db)):
+    pass
